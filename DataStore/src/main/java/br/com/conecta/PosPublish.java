c32f7be5fd8b40bbdf2f2965fd2969c531624885
JeanerSilva <jeanerufmg@gmail.com>
2018-03-29 13:14:17 -0300

    Pos

--- DataStore/src/main/java/br/com/conecta/PosPublish.java
+++ DataStore/src/main/java/br/com/conecta/PosPublish.java
@@ -18,7 +18,7 @@ public class PosPublish extends HttpServlet {
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		String pos = request.getParameter("pos") == null ? "0.0,0.0" : request.getParameter("pos") ;
+		String pos = request.getParameter("pos") == null ? "1.1,1.1" : request.getParameter("pos") ;
 		String entity = request.getParameter("entity") == null ? "unknowtracker" : request.getParameter("entity");
 
 		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
